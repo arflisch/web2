8c5f8f539eede4c42a3ed807d80f8de930726852
arnaud <arnaud.flisch@outlook.com>
2023-09-27 15:42:04 +0200

    fin ex1.3 rest client

--- exercices/1.3/routes/films.js
+++ exercices/1.3/routes/films.js
@@ -6,14 +6,14 @@ const FILMS = [
   {
     id: 1,
     title: 'Fast and Furious',
-    duration: 120,
+    duration: 110,
     budget: 150,
     link: 'https://fr.wikipedia.org/wiki/Fast_and_Furious_(s%C3%A9rie_de_films)',
   },
   {
     id: 2,
     title: 'avatar',
-    duration: 110,
+    duration: 120,
     budget: 75,
     link: 'https://fr.wikipedia.org/wiki/Avatar_(film,_2009)',
   },
@@ -27,27 +27,24 @@ const FILMS = [
   {
     id: 4,
     title: 'Status Update',
-    duration: 90,
+    duration: 94,
     budget: 5,
     link: 'https://en.wikipedia.org/wiki/Status_Update',
   },
 ];
 
-router.get('/', function(req, res, next) {
-  console.log('GET /films');
-  res.json(FILMS);
-});
+
 
 router.get('/', (req, res, next) => {
   const orderByTime =
-    req?.query?.['minimum-duration']?.includes('asc')
+    req?.query['minimum-duration']?.includes('asc')
       ? req.query['minimum-duration']
       : undefined;
   let orderedFilms;
   console.log(`order by ${orderByTime ?? 'not requested'}`);
   if (orderByTime)
-    orderedFilms = [...FILMS].sort((a, b) => a.duration.localeCompare(b.duration));
-  if (orderByTime === 'desc') orderedFilms = orderedFilms.reverse();
+    orderedFilms = [...FILMS].sort((a, b) => a.duration - b.duration);
+  if (orderByTime === '-asc') orderedFilms = orderedFilms.reverse();
 
   console.log('GET /film');
   res.json(orderedFilms ?? FILMS);
@@ -62,5 +59,30 @@ router.get('/:id', (req, res) => {
 
   res.json(FILMS[indexOfFilmFound]);
 });
+
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+  const duration = req?.body?.duration > 0 ? req.body.duration : undefined;
+  const budget = req?.body?.budget > 0 ? req.body.budget : undefined;
+  const link = req.body.link;
+
+  if (!title || !duration || !budget) return res.sendStatus(400);
+
+  const lastItemIndex = FILMS?.length !== 0 ? FILMS.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? FILMS[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newFilm = {
+    id: nextId,
+    title: title,
+    duration: duration,
+    budget: budget,
+    link: link
+  }
+
+  FILMS.push(newFilm);
+
+  res.json(newFilm);
+})
 
 module.exports = router;
